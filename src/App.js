acd77897e01caba7c1b1749dd71e46ae1f1e405a
Kushal Agrawal <agrawalskushal@gmail.com>
2023-10-06 12:27:34 +0530

    Stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const characterSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+function getRandomCharacter() {
+  return characterSet[Math.floor(Math.random() * characterSet.length)];
+}
+
 function App() {
+  const [matrixRows, setMatrixRows] = useState([]);
+  const [showText, setShowText] = useState(false);
+  const [displayText, setDisplayText] = useState('');
+  const textToType = " You got hacked. IP collecting..."; // Updated text
+
+  useEffect(() => {
+    const numRows = Math.floor(window.innerHeight / 20);
+    const numCols = Math.floor(window.innerWidth / 15);
+
+    const matrix = Array.from({ length: numRows }, () =>
+      Array.from({ length: numCols }, () => getRandomCharacter())
+    );
+
+    setMatrixRows(matrix);
+
+    const textTimeout = setTimeout(() => {
+      setShowText(true);
+      startTypingEffect();
+    }, 5000);
+
+    const matrixInterval = setInterval(() => {
+      const rowIndex = Math.floor(Math.random() * numRows);
+      const colIndex = Math.floor(Math.random() * numCols);
+      matrix[rowIndex][colIndex] = getRandomCharacter();
+      setMatrixRows([...matrix]);
+    }, 100);
+
+    return () => {
+      clearInterval(matrixInterval);
+      clearTimeout(textTimeout);
+    };
+  }, []);
+
+  const startTypingEffect = () => {
+    const textArray = textToType.split('');
+    let currentIndex = 0;
+
+    const typingInterval = setInterval(() => {
+      if (currentIndex < textArray.length) {
+        setDisplayText((prevText) => prevText + textArray[currentIndex]);
+        currentIndex++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, 100);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="matrix-container">
+      {matrixRows.map((row, rowIndex) => (
+        <div key={rowIndex} className="matrix-row">
+          {row.map((char, colIndex) => (
+            <span key={colIndex} className="matrix-character">
+              {char}
+            </span>
+          ))}
+        </div>
+      ))}
+      {showText && (
+        <div className="text-typing">
+          {displayText}
+          <span></span>
+        </div>
+      )}
     </div>
   );
 }
